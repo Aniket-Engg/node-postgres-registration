08d14c7478f579b039ebce24717d6e1a506c814c
Anthony N. Simon <anthonynsimon@users.noreply.github.com>
2016-02-17 10:47:14 +0100

    Added init file to automate OSX deployment

---
+++ bin/initOSX.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+pwd=`pwd`
+osascript -e "tell application \"Terminal\" to do script \"cd $pwd; cd ./../; clear; mongod --dbpath ./tests/test-db; \""
+osascript -e "tell application \"Terminal\" to do script \"cd $pwd; cd ./../; clear; mongo \""
+osascript -e "tell application \"Terminal\" to do script \"cd $pwd; cd ./../; clear; nodemon index \""
